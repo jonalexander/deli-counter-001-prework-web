49dc2a586056e63414e5663eff0135757dcf94fc
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-18 12:58:43 -0400

    Updated now_serving method.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,28 @@
 katz_deli = []
 
 def line(katz_deli)
-	if katz_deli.any?
-		line_msg = "The line is currently:"l
-		katz_deli.each_with_index { |name, i| line_msg << " #{i + 1}. #{name}" }		
-		puts line_msg
-	else
-		puts "The line is currently empty."
-	end
-end 		
+ if katz_deli.any?
+  line_msg = "The line is currently:"
+  katz_deli.each_with_index { |name, i| line_msg << " #{i + 1}. #{name}" }  
+  puts line_msg
+ else
+  puts "The line is currently empty."
+ end
+end   
 
 
 def take_a_number(katz_deli, name)
-	katz_deli << name
-	puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+ katz_deli << name
+ puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 
 def now_serving(katz_deli)
-	if katz_deli.any?
-		puts "Currently serving #{katz_deli.first}."
-		katz_deli.shift
-	else
-		puts "There is nobody waiting to be served!"
-	end
+ if katz_deli.any?
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+ else
+  puts "There is nobody waiting to be served!"
+ end
 end
 
